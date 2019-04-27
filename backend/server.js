372caa228d207db3d9c37cb3a62d261032a173e2
ViVaHa <varshath@Vishnus-MBP.fios-router.home>
2019-04-27 00:05:25 -0400

    Connecting to mongo

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require('cors');
+const db = require('../keys').mongoURI;
+//console.log(db);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 app.listen(port, () => console.log(`Listening on port ${port}`));
