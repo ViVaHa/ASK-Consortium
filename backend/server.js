8def0f2117f87822806f8fbe5ba230c195430a79
ViVaHa <varshath@Vishnus-MBP.fios-router.home>
2019-04-27 00:05:53 -0400

    Connecting to mongo

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 var cors = require('cors');
+const mongoose = require('mongoose');
 const db = require('../keys').mongoURI;
 //console.log(db);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.listen(port, () => console.log(`Listening on port ${port}`));
+mongoose.connect(db, {useNewUrlParser:true}).then(
+  () => {console.log("Connected to MongoDB")},
+  err => {console.error("Cannot connect to MongoDB")}
+);
