07458108a934d2c4fe14712d5cf0756dccf6f0e1
ViVaHa <varshath@Vishnus-MBP.fios-router.home>
2019-04-28 16:58:08 -0400

    Pushing code for DB switch

--- backend/DBConnections.js
+++ backend/DBConnections.js
@@ -1,9 +1,9 @@
 const db = require('../keys').mongoURI;
 const mongoose = require('mongoose');
-var dbConnectionA = mongoose.createConnection(db, {useNewUrlParser: true});
-var dbConnectionB = dbConnectionA.useDb('AirlineB');
-var dbConnectionC = dbConnectionA.useDb('AirlineC');
-var dbConnectionConsortium = dbConnectionA.useDb('Consortium');
+var dbConnectionConsortium = mongoose.createConnection(db, {useNewUrlParser: true});
+var dbConnectionB = dbConnectionConsortium.useDb('AirlineB');
+var dbConnectionC = dbConnectionConsortium.useDb('AirlineC');
+var dbConnectionA = dbConnectionConsortium.useDb('AirlineA');
 var obj={}
 obj.dbA = dbConnectionA;
 obj.dbB = dbConnectionB;
