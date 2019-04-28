7ac29f6cf48027a05834a3a9033c18b9314eb49f
ViVaHa <varshath@Vishnus-MBP.fios-router.home>
2019-04-28 17:58:45 -0400

    Adding functionality to support the three hardcoded DB's

--- backend/Routes/ticketRoutes.js
+++ backend/Routes/ticketRoutes.js
@@ -3,14 +3,30 @@ const router = express.Router();
 const key = require('../../keys');
 const ticketDetails = require('../models/ticketDetails')
 
+
+const getModel=function(airline){
+  if(airline==='Airline 1'){
+    model = ticketDetails.ticketDetailsA;
+  }else if(airline == 'Airline 2'){
+    model = ticketDetails.ticketDetailsB;
+  }else{
+    model = ticketDetails.ticketDetailsC;
+  }
+  return model;
+}
+
+
 router.post('/add/', (req,res) =>{
     console.log(req.body.name);
-    ticketDetails.findOne({$and : [{customer_name:req.body.customer_name}, {airline_name : req.body.airline_name}, {flight_name:req.body.flight_name}]})
+    let airline = req.body.airline_name;
+    var model = getModel(airline);
+
+    model.findOne({$and : [{customer_name:req.body.customer_name}, {airline_name : req.body.airline_name}, {flight_name:req.body.flight_name}]})
       .then(details =>{
         if(details){
           res.status(400).json({error:' Already added'}, {errorCode : 2});
         }else{
-          const newticketDetails = new ticketDetails({
+          const newticketDetails = new model({
             customer_name: req.body.customer_name,
             airline_name : req.body.airline_name,
             flight_name: req.body.flight_name,
@@ -27,8 +43,10 @@ router.post('/add/', (req,res) =>{
   });
 
   router.get('/customers', (req, res)=>{
+    let airline = req.query.airline_name;
+    var model = getModel(airline);
     console.log("Request for tickets/customers", req.query);
-    ticketDetails.find({airline_name : req.query.airline_name, flight_name:req.query.flight_name},function(err,details){
+    model.find({airline_name : req.query.airline_name, flight_name:req.query.flight_name},function(err,details){
         if(err){
             console.log(err);
         } else {
@@ -39,7 +57,9 @@ router.post('/add/', (req,res) =>{
 
   router.get('/flights', (req, res)=>{
     console.log("Request for tickets/flights", req.query);
-    ticketDetails.distinct('flight_name',{airline_name : req.query.airline_name},function(err,details){
+    let airline = req.query.airline_name;
+    var model = getModel(airline);
+    model.distinct('flight_name',{airline_name : req.query.airline_name},function(err,details){
         if(err){
             console.log(err);
         } else {
@@ -60,27 +80,60 @@ router.post('/add/', (req,res) =>{
   });
 
   router.put('/update', (req,res) =>{
-    console.log(req.body);
-    ticketDetails.updateOne({$and:[{ customer_name : req.body.customer_name}, {airline_name: req.body.old_airline_name},{ flight_name: req.body.old_flight_name } ]} ,{ $set: {airline_name: req.body.new_airline_name, flight_name: req.body.new_flight_name } },   function(err, success){
-        if(err){
-          console.log(err);
-        }else{
-            if(success.n>0) {
+    let old_airline = req.query.old_airline_name;
+    var old_model = getModel(old_airline);
+    let new_airline = req.query.new_airline_name;
+    var new_model = getModel(new_airline);
+    // model.updateOne({$and:[{ customer_name : req.body.customer_name}, {airline_name: req.body.old_airline_name},{ flight_name: req.body.old_flight_name } ]} ,{ $set: {airline_name: req.body.new_airline_name, flight_name: req.body.new_flight_name } },   function(err, success){
+    //     if(err){
+    //       console.log(err);
+    //     }else{
+    //         if(success.n>0) {
+    //
+    //           res.status(200).json({"success" : true });
+    //         }
+    //
+    //         else
+    //             res.status(200).json({"Error": "No such ticket", errorCode:3})
+    //     }
+    // });
+    //
+    //
+    //
 
-              res.status(200).json({"success" : true });
-            }
+    const newTicket = new new_model({
+      customer_name : req.query.customer_name,
+      airline_name : new_airline,
+      flight_name : req.query.new_flight_name
+    })
+    newTicket.save()
+    .then((ticket)=>{
+      console.log(ticket);
+      old_model.deleteOne({$and :[{customer_name: req.query.customer_name}, {flight_name: req.query.old_flight_name}, {airline_name:req.query.old_airline_name}]})
+      .then(()=>{
+        res.status(200).json({"success" : true });
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
 
-            else
-                res.status(200).json({"Error": "No such ticket", errorCode:3})
-        }
-    });
+
+
+
+
   })
 
 
 
   router.get('/validateTicket', (req, res)=>{
-    console.log(req.body);
-    ticketDetails.findOne({$and : [{ airline_name : req.body.airline_name},{flight_name : req.body.flight_name}, {customer_name: req.body.customer_name} ]},function(err,details){
+    console.log(req.query);
+    let airline = req.body.airline_name;
+    var model = getModel(airline);
+    model.findOne({$and : [{ airline_name : req.body.airline_name},{flight_name : req.body.flight_name}, {customer_name: req.body.customer_name} ]},function(err,details){
         if(err){
             console.log(err);
 
